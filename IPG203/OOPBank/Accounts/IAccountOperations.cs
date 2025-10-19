663252af69666e50a27f7b70e6056e2bcdfb83aa
HadeelAlkhatib22 <hdyl56611@gmail.com>
2025-10-19 09:02:59 -0700

    Hadeel: Upload IAccountOperations, SavingsAccount

---
+++ IPG203/OOPBank/Accounts/IAccountOperations.cs
@@ -0,0 +1,10 @@
+﻿namespace OOPBankDemo
+{
+    // إستخدام ميزة الواجهة
+    public interface IAccountOperations
+    {
+        void Deposit(decimal amount); // ايداع
+        void Withdraw(decimal amount); //  سحب
+        void ApplyMonthlyUpdate();  // تطبيق التحديثات الشهرية
+    }
+}
